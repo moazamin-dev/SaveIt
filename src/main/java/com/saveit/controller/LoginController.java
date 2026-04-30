bdb3aeefd7142f976018fc573f8515ca15f56a62
Moaz Amin <moazamin174@gmail.com>
2026-04-30 22:29:49 +0300

    deleted un wanted items

--- src/main/java/com/saveit/controller/LoginController.java
+++ src/main/java/com/saveit/controller/LoginController.java
@@ -54,11 +54,4 @@ public class LoginController extends Controller {
     private void displayError(String message) {
         errorLabel.setText(message);
     }
-//    private void showError(String message) {
-//        Alert alert = new Alert(Alert.AlertType.ERROR);
-//        alert.setTitle("Error");
-//        alert.setHeaderText(null);
-//        alert.setContentText(message);
-//        alert.showAndWait();
-//    }
 }
